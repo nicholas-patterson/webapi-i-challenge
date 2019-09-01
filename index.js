8130ed6e1fa68a222a5624ebb84c719201a8d4c4
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-01 15:22:22 -0400

    :sparkles: get by id and get list and delete working
    get by id and get list and delete working

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
 // implement your API here
+const express = require("express");
+const server = express();
+const Db = require("./data/db");
+server.use(express.json());
+
+// Get Method
+
+server.get("/api/users", (req, res) => {
+  Db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "The user information could not be retrieved" });
+    });
+});
+
+// Post Method
+
+server.post("/api/users", (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    res.status(400).json({ message: "Please provide name and bio for user" });
+  } else {
+    const userInformation = req.body;
+    Db.insert(userInformation)
+      .then(result => {
+        res.status(201).json(result);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ message: "There was a error saving user to the database" });
+      });
+  }
+});
+
+// Delete Method
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  Db.remove(id)
+    .then(result => {
+      if (result) {
+        res.status(200).json({ message: "The user was successfully removed" });
+      } else {
+        res
+          .status(404)
+          .json({ message: "User with the specified ID was not found" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "The user could not be removed" });
+    });
+});
+
+// Get Specific User
+
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  Db.findById(id)
+    .then(result => {
+      console.log(result);
+      if (!result) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist"
+        });
+      } else {
+        res.status(200).json(result);
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "The user information could not be retrieved" });
+    });
+});
+
+const port = 5050;
+server.listen(port, () => console.log(`Server Listening On Port: ${port}`));
